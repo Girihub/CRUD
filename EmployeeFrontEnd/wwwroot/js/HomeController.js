cd7d1998628587a70cae54bfcabc878d01d6894e
Girihub <giridhardandikwar@gmail.com>
2019-11-17 15:05:54 +0530

    commit

---
+++ EmployeeFrontEnd/wwwroot/js/HomeController.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    getData();
+});
+
+function getData() {
+    $.ajax({
+        url: 'https://localhost:44376/api/Employee',
+        dataType: "json",
+        method: 'GET',
+        success: function (data) {
+            var employeeTable = $('#displayTable tbody');
+            employeeTable.empty();
+
+            $(data).each(function (index, emp) {
+                employeeTable.append('<tr><td>' + emp.FirstName + '</td><td>'
+                    + emp.LastName + '</td><td>' + emp.EmpAddress + '</td><td>'
+                    + emp.MobileNumber + '</td><td>' + emp.ZipCode + '</td></tr>');
+            });
+        }
+    });
+}
